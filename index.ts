2a86a016975b913ea4f0316ed4384807a988e921
Artyom503 <78357397+Artyom503@users.noreply.github.com>
2022-02-19 17:05:20 +0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve('../dist/client_build', "index.html"));
+    res.sendFile(path.resolve('../', "index.html"));
   });
 }
 
