650d08be1d1bdc4212e54f575cadf6902ca654ec
Artyom503 <78357397+Artyom503@users.noreply.github.com>
2022-02-19 17:29:20 +0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-      app.use(express.static(path.resolve(__dirname, '../client_build')));
+      app.use(express.static("../client_build"));
       app.get("*", (req, res) => {
-    res.sendFile(path.resolve('../client_build', "index.html"));
+    res.sendFile(path.resolve(__dirname, '../client_build', "index.html"));
   });
       
     }
